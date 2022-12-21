f8860d637f6580883a53ed9ea5723dc18e174f30
PetxCode <brighterdayscodelab@gmail.com>
2022-12-21 01:41:27 +0100

    gql

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import InputComp from "./component/InputComp";
+import ViewComp from "./component/ViewComp";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-function App() {
+const App = () => {
+  const client = new ApolloClient({
+    uri: "http://localhost:2244/api",
+    cache: new InMemoryCache(),
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ApolloProvider client={client}>
+        <br />
+        <br />
+        <InputComp />
+        <br />
+        <ViewComp />
+      </ApolloProvider>
     </div>
   );
-}
+};
 
 export default App;
